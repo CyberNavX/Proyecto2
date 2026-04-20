1d75a946ea7c1e21ea900ba6e1fc8960348c9845
CyberNavX <gerbergarciasalas52@gmail.com>
2026-04-19 18:45:10 -0600

    Primera parte de AFD

--- AFD.java
+++ AFD.java
@@ -2,14 +2,58 @@
 // Debe implementar todos los metodos aqui listados.
 // No cambie la firma del constructor ni de los metodos ya creados.
 // Puede crear cualquier metodo extra que desee
+import java.io.File;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class AFD {
-    private static final String stringFormat = "Alphabet: %s, Final States: %s, Error States: %s, Minimum: %b";
 
+    private String[] alfabeto;
+    private int cantidadEstados;
+    private int estadosFinales;
+    private int[][] matrizTransicion;
+
+    private static final String stringFormat = "Alphabet: %s, Final States: %s, Error States: %s, Minimum: %b";
+    
     public AFD(String path) {
         // Lea el archivo y genere su estructura de AFD
-    }
+        try{
+            File archivo = new File(path);
+            Scanner leer = new Scanner(archivo);
+
+            if (leer.hasNextLine()) {
+                this.alfabeto = leer.nextLine().split(",");
+            }
+
+            if (leer.hasNextLine()) {
+                this.cantidadEstados = Integer.parseInt(leer.nextLine().trim());
+            }
+
+            if (leer.hasNextLine()) {
+                String[] textoFinal = leer.nextLine().split(",");
+                this.estadosFinales = new int[textoFinal.length];
+                for (int i=0; i < textoFinal.length; i++){
+                    this.estadosFinales[i] = Integer.parseInt(textoFinal[i].trim());
+
+                }
+            }
+
+            for (int i=0; i< this.cantidadEstados; i++){
+                if (leer.hasNextLine()) {
+                    String[] fila = leer.nextLine().split(",");
+                    for (int j=0; j < this.alfabeto.length; j++){
+                        this.matrizTransicion[i][j] = Integer.parseInt(fila[j].trim());
+                    }
+                    
+                }
+            }
+            leer.close();
+
+        }catch (Exception e){
+
+        }
+
+    } 
 
     public boolean accept(String str) {
         // Revisa si la cuerda str es aceptada o no por el AFD
